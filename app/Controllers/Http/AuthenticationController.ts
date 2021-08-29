b926664845be1b38a2c11f6dbdbd99e8aa8ab397
Risha Sringa Changmai <michael.the.don@gmail.com>
2021-08-29 14:35:32 +0530

    shifted to prisma

--- app/Controllers/Http/AuthenticationController.ts
+++ app/Controllers/Http/AuthenticationController.ts
@@ -6,8 +6,14 @@ import Package from "App/Models/Package";
 import jwt from 'jsonwebtoken'
 import {jwtconfig} from "Config/jwtconfig";
 import {HttpException} from "@adonisjs/http-server/build/src/Exceptions/HttpException";
+import {PrismaClient} from "@prisma/client";
 
 export default class AuthenticationController {
+  private client: PrismaClient
+
+  constructor() {
+    this.client = new PrismaClient();
+  }
 
   public async startLicense(ctx: HttpContextContract) {
     const {name, email, password, licenceClassType, licenseTimerType, pkg} = ctx.request.body()
@@ -33,21 +39,15 @@ export default class AuthenticationController {
 
   public async loginClient(ctx: HttpContextContract) {
     const {email, password} = ctx.request.body();
-    const user = await User.findByOrFail('email', email);
+    const user = await this.client.users.findUnique({where: { email }, include: {licenses_licenses_ownerTousers: true, licenses_licenses_employeeLicenseIdTousers: true}});
     const pwd = new SecurePassword()
     if (user) {
       const passwordBuffer = Buffer.from(password)
-      const hashBuff = Buffer.from(user.password, 'base64')
+      const hashBuff = Buffer.from(user.password!, 'base64')
       const validity = await pwd.verify(passwordBuffer, hashBuff);
       console.log(validity)
       if (validity === SecurePassword.VALID) {
-        const license = await License.findBy('owner', user.id);
-        const employeeLicense = await License.findBy('employeeLicenseId', user.id);
-        return {
-          user,
-          license,
-          employeeLicense
-        };
+        return user;
       } else {
         throw HttpException.invoke({}, 401, "Invalid email or password")
       }
